dc421afa22744706fb7629b76c108759efa6c991
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2026-03-19 18:14:27 +0000

    fix: set zsh directory to user's home directory

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 git clone https://github.com/shaksternano/debian-workspace ~/.workspace-setup
 sudo apt install -y git curl wget build-essential zip zsh zsh-autosuggestions zsh-syntax-highlighting
+export ZSH=$HOME/.oh-my-zsh
 sudo chsh -s $(which zsh) $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp ~/.workspace-setup/templates/.zshrc ~/
