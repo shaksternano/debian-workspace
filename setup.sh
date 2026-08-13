b80359b08686a882158c1e886e6dd7939f8e7a01
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2026-08-13 18:54:12 +0100

    feat: install screen and tmux

--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@
 sudo apt update
 sudo apt upgrade -y
 git clone https://github.com/shaksternano/debian-workspace ~/.workspace-setup
-sudo apt install -y git curl wget build-essential zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo apt install -y git curl wget build-essential zip zsh zsh-autosuggestions zsh-syntax-highlighting screen tmux
 export ZSH=$HOME/.oh-my-zsh
 sudo chsh -s $(which zsh) $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp ~/.workspace-setup/templates/.zshrc ~/
 mkdir -p ~/.oh-my-zsh/custom/themes/
 cp ~/.workspace-setup/templates/debian.zsh-theme ~/.oh-my-zsh/custom/themes/
+cp ~/.workspace-setup/templates/.tmux.conf ~/
 zsh
